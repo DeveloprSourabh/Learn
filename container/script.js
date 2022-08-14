0b617e1ba61baa8aa5a0bb2b0b726fb6bf2fffa4
Sourabh <developr.sourabh@gmail.com>
2022-08-14 16:48:37 +0530

    craete new folder and file

--- container/script.js
+++ container/script.js
@@ -4,5 +4,5 @@ namaste.addEventListener('click',inputMsg);
 
 function inputMsg(){
 let name = prompt("Enter Your Name?")
-   namaste.textContent = "Roll No. 1:" + name;
+   namaste.textContent = "Roll No. 1:" +  name;
 }
